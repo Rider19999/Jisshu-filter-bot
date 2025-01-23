29fc31efe03d1305397d8268ef8fb24af5f8ecde
Aniket Kumar <140139292+Rider19999@users.noreply.github.com>
2025-01-23 20:07:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,11 +69,11 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg')).split()
+START_IMG = (environ.get('START_IMG', 'https://iili.io/2sBle1I.jpg https://iili.io/2sBlUes.jpg https://iili.io/2sBlD7e.jpg https://iili.io/2sB02EB.jpg https://iili.io/2sB0nTJ.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
+PAYPICS = (environ.get('PAYPICS', 'https://iili.io/2s3DbTu.jpg')).split()
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://iili.io/2sBAGtI.jpg'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://enthusiastic-odilia-gokubot123-2803451d.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
