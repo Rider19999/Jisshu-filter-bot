2434f70ff248030ddc7031cf81ba832ea3aac56d
Aniket Kumar <140139292+Rider19999@users.noreply.github.com>
2025-01-23 20:13:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://enthusiastic-odilia-gokubot123-2803451d.koyeb.app/")
+URL = environ.get("FQDN", "")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
